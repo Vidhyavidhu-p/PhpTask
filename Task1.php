6bf3bd8003248d79213b884b3fa3d12854676077
Vidhya0017 <148936906+Vidhyavidhu-p@users.noreply.github.com>
2025-11-07 11:09:36 +0530

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,12 @@
+<?php 
+echo "Good morning! Have a great day!<br>";
+echo "Always believe in yourself.<br>";
+echo "Success comes with hard work.<br>";
+echo "Stay positive and keep learning.<br>";
+echo "Every day is a new beginning.<br>";
+echo "Be kind and spread happiness.<br>";
+echo "Dream big and never give up.<br>";
+echo "Confidence is the key to success.<br>";
+echo "Learn something new every day.";
+echo "Smile — it makes life beautiful!<br>";
+?>
---
+++ Task2.php
@@ -0,0 +1,38 @@
+<!Doctype html>
+<head><title>Second Task</title>
+<style>
+    h1{
+        background-color: lightcoral;
+        color:black;
+        text-align: center;}
+        h2{
+            text-align: center;
+           color:black;
+        }
+    
+    body{
+        background-color: beige
+    }
+</style>
+</head>
+<body>
+    <h1>Data types</h1></body>
+    <h2>1.Integer data type:<br><?php 
+    $a=20;
+    $b=30;
+     var_dump ($a) 
+       ?>
+       </h2>
+
+    <h2>2.String data type: <br><?php 
+    $name="kaviya";
+    var_dump($name)
+    ?>
+    </h2>
+
+      <h2>3.Float data type:<br><?php 
+    $x=10.5;
+        var_dump(value: $x)
+        ?>
+    </h2>
+       </html>
